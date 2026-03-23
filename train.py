3a2141f0ffa87febfd3cebe63ad26d0573d07b4a
namanjain2020 <153709313+namanjain2020@users.noreply.github.com>
2026-03-24 01:01:20 +0530

    train.py

--- train.py
+++ train.py
@@ -6,16 +6,16 @@ from attention_rnn import AttentionRNN
 from bilstm_model import BiLSTM
 
 with open("TrainingNames.txt") as f:
-     names = [line.strip().lower() for line in f]
+     names = [line.strip().lower() for line in f]     # remove spaces + lowercase
 
-names = ["<" + name + ">" for name in names]
+names = ["<" + name + ">" for name in names]          # Add start "<" and end ">" tokens to each name
 
-chars = sorted(list(set("".join(names))))
-char2idx = {c:i for i,c in enumerate(chars)}
-idx2char = {i:c for c,i in char2idx.items()}
-vocabulary_size = len(chars)
+chars = sorted(list(set("".join(names))))             # Get all unique characters
+char2idx = {c:i for i,c in enumerate(chars)}          # Create character-to-index mappings
+idx2char = {i:c for c,i in char2idx.items()}          # Create index-to-character mappings
+vocabulary_size = len(chars)                          # total unique characters
 
-def encoding(name):
+def encoding(name):                                   # Convert a name (string) into list of indices
     return [char2idx[c] for c in name]
 
 MODEL = "attention"
@@ -33,22 +33,21 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr = 0.002)
 epochs = 25
 
-model.train()
-
+model.train()                                         # set model to training mode
 for epoch in range(epochs):
     total_loss = 0
     for name in names:
-        sequence = encoding(name)
-        x = torch.tensor(sequence[:-1]).unsqueeze(0)
-        y = torch.tensor(sequence[1:]).unsqueeze(0)
+        sequence = encoding(name)                     # convert name to indices
+        x = torch.tensor(sequence[:-1]).unsqueeze(0)  # Input sequence (all except last char)
+        y = torch.tensor(sequence[1:]).unsqueeze(0)   # Target sequence (all except first char)
 
-        optimizer.zero_grad()
-        output, _ = model(x)
-        loss = criterion(output.view(-1,vocabulary_size), y.view(-1))
-        loss.backward()
+        optimizer.zero_grad()                          # clear previous gradients
+        output, _ = model(x)                           # Forward pass
+        loss = criterion(output.view(-1,vocabulary_size), y.view(-1))   # Reshape output for loss computation
+        loss.backward()                                                 # Backpropagation
         optimizer.step()
         total_loss += loss.item()
     print(f"Epoch number : {epoch + 1} and Loss : {total_loss}")
 
-torch.save(model.state_dict(), MODEL + "_model.pth")
+torch.save(model.state_dict(), MODEL + "_model.pth")                   # Saving the trained model
 print("Your model has been saved successfully")
